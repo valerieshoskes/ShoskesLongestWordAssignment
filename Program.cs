aaa60304233a7c942472f72c09be06aec57a3b89
valerieshoskes <indi.vs@gmail.com>
2016-09-13 16:13:05 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoskesLongestWordAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            string[] sentenceArray = sentence.Split();
+            string longestWord = "";
+            int mostLetters = 0;
+            foreach (string word in sentenceArray)
+            {
+                if (word.Length >= mostLetters)
+                {
+                    mostLetters = word.Length;
+                    longestWord = word;
+                }
+            }
+            Console.WriteLine(longestWord);
+        }
+    }
+}
